d65ae1608cee2ddbf74b09bf1afc49df1e66ea07
Pavol Ipoth <pavol.ipoth@protonmail.com>
2026-03-17 22:12:50 +0100

    Refactor e2e tests

--- e2e/e2e_uma_test.go
+++ e2e/e2e_uma_test.go
@@ -16,16 +16,20 @@ import (
 )
 
 var _ = Describe("UMA Code Flow authorization", func() {
-	var portNum string
-	var proxyAddress string
+	var (
+		portNum      string
+		proxyAddress string
+		server       *http.Server
+	)
+
 	errGroup, _ := errgroup.WithContext(context.Background())
-	var server *http.Server
 
 	AfterEach(func() {
 		if server != nil {
 			err := server.Shutdown(context.Background())
 			Expect(err).NotTo(HaveOccurred())
 		}
+
 		if errGroup != nil {
 			err := errGroup.Wait()
 			Expect(err).NotTo(HaveOccurred())
@@ -33,12 +37,15 @@ var _ = Describe("UMA Code Flow authorization", func() {
 	})
 
 	BeforeEach(func() {
-		var err error
-		var upstreamSvcPort string
+		var (
+			err             error
+			upstreamSvcPort string
+		)
 
 		server, upstreamSvcPort = startAndWaitTestUpstream(errGroup, false, false, false)
 		portNum, err = generateRandomPort()
 		Expect(err).NotTo(HaveOccurred())
+
 		proxyAddress = localURI + portNum
 
 		proxyArgs := []string{
@@ -69,23 +76,29 @@ var _ = Describe("UMA Code Flow authorization", func() {
 		osArgs := make([]string, 0, 1+len(proxyArgs))
 		osArgs = append(osArgs, os.Args[0])
 		osArgs = append(osArgs, proxyArgs...)
+
 		startAndWait(portNum, osArgs)
 	})
 
 	When("Accessing resource, where user is allowed to access", func() {
 		It("should login with user/password and logout successfully", func(_ context.Context) {
 			var err error
+
 			rClient := resty.New()
 			rClient.SetTLSClientConfig(&tls.Config{RootCAs: caPool, MinVersion: tls.VersionTLS13})
 			resp := codeFlowLogin(rClient, proxyAddress+umaAllowedPath, http.StatusOK, testUser, testPass)
 			Expect(resp.Header().Get("Proxy-Accepted")).To(Equal("true"))
 
 			body := resp.Body()
+
 			Expect(strings.Contains(string(body), umaCookieName)).To(BeTrue())
 
 			By("Accessing not allowed path")
+
 			resp, err = rClient.R().Get(proxyAddress + umaForbiddenPath)
+
 			body = resp.Body()
+
 			Expect(err).NotTo(HaveOccurred())
 			Expect(resp.StatusCode()).To(Equal(http.StatusForbidden))
 			Expect(strings.Contains(string(body), umaCookieName)).To(BeFalse())
@@ -114,23 +127,30 @@ var _ = Describe("UMA Code Flow authorization", func() {
 	When("Accessing resource, which exists but user is not allowed and then allowed resource", func() {
 		It("should be forbidden and then allowed", func(_ context.Context) {
 			var err error
+
 			rClient := resty.New()
 			rClient.SetTLSClientConfig(&tls.Config{RootCAs: caPool, MinVersion: tls.VersionTLS13})
 			resp := codeFlowLogin(rClient, proxyAddress+umaForbiddenPath, http.StatusForbidden, testUser, testPass)
 
 			body := resp.Body()
+
 			Expect(strings.Contains(string(body), umaCookieName)).To(BeFalse())
 
 			By("Accessing allowed resource")
+
 			resp, err = rClient.R().Get(proxyAddress + umaAllowedPath)
+
 			body = resp.Body()
+
 			Expect(err).NotTo(HaveOccurred())
 			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
 			Expect(strings.Contains(string(body), umaCookieName)).To(BeFalse())
 
 			By("Accessing allowed resource one more time, checking uma cookie set")
+
 			resp, err = rClient.R().Get(proxyAddress + umaAllowedPath)
 			body = resp.Body()
+
 			Expect(err).NotTo(HaveOccurred())
 			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
 			Expect(strings.Contains(string(body), umaCookieName)).To(BeTrue())
@@ -139,16 +159,20 @@ var _ = Describe("UMA Code Flow authorization", func() {
 })
 
 var _ = Describe("UMA Code Flow authorization with method scope", func() {
-	var portNum string
-	var proxyAddress string
+	var (
+		portNum      string
+		proxyAddress string
+		server       *http.Server
+	)
+
 	errGroup, _ := errgroup.WithContext(context.Background())
-	var server *http.Server
 
 	AfterEach(func() {
 		if server != nil {
 			err := server.Shutdown(context.Background())
 			Expect(err).NotTo(HaveOccurred())
 		}
+
 		if errGroup != nil {
 			err := errGroup.Wait()
 			Expect(err).NotTo(HaveOccurred())
@@ -156,12 +180,15 @@ var _ = Describe("UMA Code Flow authorization with method scope", func() {
 	})
 
 	BeforeEach(func() {
-		var err error
-		var upstreamSvcPort string
+		var (
+			err             error
+			upstreamSvcPort string
+		)
 
 		server, upstreamSvcPort = startAndWaitTestUpstream(errGroup, false, false, false)
 		portNum, err = generateRandomPort()
 		Expect(err).NotTo(HaveOccurred())
+
 		proxyAddress = localURI + portNum
 
 		proxyArgs := []string{
@@ -194,6 +221,7 @@ var _ = Describe("UMA Code Flow authorization with method scope", func() {
 		osArgs := make([]string, 0, 1+len(proxyArgs))
 		osArgs = append(osArgs, os.Args[0])
 		osArgs = append(osArgs, proxyArgs...)
+
 		startAndWait(portNum, osArgs)
 	})
 
@@ -202,17 +230,21 @@ var _ = Describe("UMA Code Flow authorization with method scope", func() {
 			"should login with user/password, don't access forbidden resource and logout successfully",
 			func(_ context.Context) {
 				var err error
+
 				rClient := resty.New()
 				rClient.SetTLSClientConfig(&tls.Config{RootCAs: caPool, MinVersion: tls.VersionTLS13})
 				resp := codeFlowLogin(rClient, proxyAddress+umaMethodAllowedPath, http.StatusOK, testUser, testPass)
 				Expect(resp.Header().Get("Proxy-Accepted")).To(Equal("true"))
 
 				body := resp.Body()
+
 				Expect(strings.Contains(string(body), umaCookieName)).To(BeTrue())
 
 				By("Accessing not allowed method")
+
 				resp, err = rClient.R().Post(proxyAddress + umaMethodAllowedPath)
 				body = resp.Body()
+
 				Expect(err).NotTo(HaveOccurred())
 				Expect(resp.StatusCode()).To(Equal(http.StatusForbidden))
 				Expect(strings.Contains(string(body), umaCookieName)).To(BeFalse())
@@ -229,18 +261,22 @@ var _ = Describe("UMA Code Flow authorization with method scope", func() {
 })
 
 var _ = Describe("UMA no-redirects authorization with forwarding client credentials grant", func() {
-	var portNum string
-	var proxyAddress string
-	var fwdPortNum string
-	var fwdProxyAddress string
+	var (
+		portNum         string
+		proxyAddress    string
+		fwdPortNum      string
+		fwdProxyAddress string
+		server          *http.Server
+	)
+
 	errGroup, _ := errgroup.WithContext(context.Background())
-	var server *http.Server
 
 	AfterEach(func() {
 		if server != nil {
 			err := server.Shutdown(context.Background())
 			Expect(err).NotTo(HaveOccurred())
 		}
+
 		if errGroup != nil {
 			err := errGroup.Wait()
 			Expect(err).NotTo(HaveOccurred())
@@ -248,14 +284,17 @@ var _ = Describe("UMA no-redirects authorization with forwarding client credenti
 	})
 
 	BeforeEach(func() {
-		var err error
-		var upstreamSvcPort string
+		var (
+			err             error
+			upstreamSvcPort string
+		)
 
 		server, upstreamSvcPort = startAndWaitTestUpstream(errGroup, false, false, false)
 		portNum, err = generateRandomPort()
 		Expect(err).NotTo(HaveOccurred())
 		fwdPortNum, err = generateRandomPort()
 		Expect(err).NotTo(HaveOccurred())
+
 		proxyAddress = localURI + portNum
 		fwdProxyAddress = httpLocalURI + fwdPortNum
 
@@ -310,10 +349,13 @@ var _ = Describe("UMA no-redirects authorization with forwarding client credenti
 		osArgs := make([]string, 0, 1+len(proxyArgs))
 		osArgs = append(osArgs, os.Args[0])
 		osArgs = append(osArgs, proxyArgs...)
+
 		startAndWait(portNum, osArgs)
+
 		fwdOsArgs := make([]string, 0, 1+len(fwdProxyArgs))
 		fwdOsArgs = append(fwdOsArgs, os.Args[0])
 		fwdOsArgs = append(fwdOsArgs, fwdProxyArgs...)
+
 		startAndWait(fwdPortNum, fwdOsArgs)
 	})
 
@@ -327,18 +369,23 @@ var _ = Describe("UMA no-redirects authorization with forwarding client credenti
 			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
 
 			body := resp.Body()
+
 			Expect(strings.Contains(string(body), umaFwdMethodAllowedPath)).To(BeTrue())
 
 			By("Accessing resource without access for client id")
+
 			resp, err = rClient.R().Get(proxyAddress + umaAllowedPath)
 			body = resp.Body()
+
 			Expect(err).NotTo(HaveOccurred())
 			Expect(resp.StatusCode()).To(Equal(http.StatusForbidden))
 			Expect(strings.Contains(string(body), umaAllowedPath)).To(BeFalse())
 
 			By("Accessing not allowed method")
+
 			resp, err = rClient.R().Post(proxyAddress + umaFwdMethodAllowedPath)
 			body = resp.Body()
+
 			Expect(err).NotTo(HaveOccurred())
 			Expect(resp.StatusCode()).To(Equal(http.StatusForbidden))
 			Expect(strings.Contains(string(body), umaFwdMethodAllowedPath)).To(BeFalse())
@@ -347,18 +394,22 @@ var _ = Describe("UMA no-redirects authorization with forwarding client credenti
 })
 
 var _ = Describe("UMA no-redirects authorization with forwarding direct access grant", func() {
-	var portNum string
-	var proxyAddress string
-	var fwdPortNum string
-	var fwdProxyAddress string
+	var (
+		portNum         string
+		proxyAddress    string
+		fwdPortNum      string
+		fwdProxyAddress string
+		server          *http.Server
+	)
+
 	errGroup, _ := errgroup.WithContext(context.Background())
-	var server *http.Server
 
 	AfterEach(func() {
 		if server != nil {
 			err := server.Shutdown(context.Background())
 			Expect(err).NotTo(HaveOccurred())
 		}
+
 		if errGroup != nil {
 			err := errGroup.Wait()
 			Expect(err).NotTo(HaveOccurred())
@@ -366,14 +417,17 @@ var _ = Describe("UMA no-redirects authorization with forwarding direct access g
 	})
 
 	BeforeEach(func() {
-		var err error
-		var upstreamSvcPort string
+		var (
+			err             error
+			upstreamSvcPort string
+		)
 
 		server, upstreamSvcPort = startAndWaitTestUpstream(errGroup, false, false, false)
 		portNum, err = generateRandomPort()
 		Expect(err).NotTo(HaveOccurred())
 		fwdPortNum, err = generateRandomPort()
 		Expect(err).NotTo(HaveOccurred())
+
 		proxyAddress = localURI + portNum
 		fwdProxyAddress = httpLocalURI + fwdPortNum
 
@@ -430,11 +484,13 @@ var _ = Describe("UMA no-redirects authorization with forwarding direct access g
 		osArgs := make([]string, 0, 1+len(proxyArgs))
 		osArgs = append(osArgs, os.Args[0])
 		osArgs = append(osArgs, proxyArgs...)
+
 		startAndWait(portNum, osArgs)
 
 		fwdOsArgs := make([]string, 0, 1+len(fwdProxyArgs))
 		fwdOsArgs = append(fwdOsArgs, os.Args[0])
 		fwdOsArgs = append(fwdOsArgs, fwdProxyArgs...)
+
 		startAndWait(fwdPortNum, fwdOsArgs)
 	})
 
@@ -448,16 +504,18 @@ var _ = Describe("UMA no-redirects authorization with forwarding direct access g
 			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
 
 			body := resp.Body()
+
 			Expect(strings.Contains(string(body), umaMethodAllowedPath)).To(BeTrue())
 			Expect(resp.Header().Get(constant.UMAHeader)).NotTo(BeEmpty())
 
 			By("Repeating access to allowed resource, we verify that uma was saved and reused")
+
 			resp, err = rClient.R().Get(proxyAddress + umaMethodAllowedPath)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
 
 			body = resp.Body()
-			GinkgoLogr.Info(string(body))
+
 			Expect(strings.Contains(string(body), umaMethodAllowedPath)).To(BeTrue())
 			Expect(resp.Header().Get(constant.UMAHeader)).To(BeEmpty())
 			// as first request should return uma token in header, it should be
@@ -466,16 +524,20 @@ var _ = Describe("UMA no-redirects authorization with forwarding direct access g
 			Expect(strings.Contains(string(body), constant.UMAHeader)).To(BeTrue())
 
 			By("Accessing resource without access for user")
+
 			resp, err = rClient.SetTimeout(1 * time.Hour).R().Get(proxyAddress + umaForbiddenPath)
 			body = resp.Body()
+
 			Expect(err).NotTo(HaveOccurred())
 			Expect(resp.StatusCode()).To(Equal(http.StatusForbidden))
 			Expect(strings.Contains(string(body), umaForbiddenPath)).To(BeFalse())
 			Expect(resp.Header().Get(constant.UMATicketHeader)).NotTo(BeEmpty())
 
 			By("Accessing not allowed method")
+
 			resp, err = rClient.R().Post(proxyAddress + umaMethodAllowedPath)
 			body = resp.Body()
+
 			Expect(err).NotTo(HaveOccurred())
 			Expect(resp.StatusCode()).To(Equal(http.StatusForbidden))
 			Expect(strings.Contains(string(body), umaMethodAllowedPath)).To(BeFalse())
@@ -484,15 +546,20 @@ var _ = Describe("UMA no-redirects authorization with forwarding direct access g
 })
 
 var _ = Describe("UMA Code Flow, NOPROXY authorization with method scope", func() {
-	var portNum string
-	var proxyAddress string
+	var (
+		portNum      string
+		proxyAddress string
+	)
+
 	umaCookieName := "TESTUMACOOKIE"
 	// server := httptest.NewServer(&testsuite.FakeUpstreamService{})
 
 	BeforeEach(func() {
 		var err error
+
 		portNum, err = generateRandomPort()
 		Expect(err).NotTo(HaveOccurred())
+
 		proxyAddress = localURI + portNum
 
 		proxyArgs := []string{
@@ -530,6 +597,7 @@ var _ = Describe("UMA Code Flow, NOPROXY authorization with method scope", func(
 	When("Accessing allowed resource", func() {
 		It("should be allowed and logout successfully", func(_ context.Context) {
 			var err error
+
 			rClient := resty.New()
 			rClient.SetTLSClientConfig(&tls.Config{RootCAs: caPool, MinVersion: tls.VersionTLS13})
 			rClient.SetHeaders(map[string]string{
